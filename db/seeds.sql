0d68806d27cccbd2e51973821a2444b391433b3a
Josh Faison <Joshfaiso@yahoo.com>
2020-02-13 12:41:35 -0500

    setting up components in config folder

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burger(burger_name, devoured)
+VALUES("hamburger", TRUE);
+
+INSERT INTO burger(burger_name, devoured)
+VALUES("cheeseburger", TRUE);
+
+INSERT INTO burger(burger_name, devoured)
+VALUES("turkey burger", FALSE);
+
