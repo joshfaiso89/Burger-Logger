58aeee7a5e9c6939a6ac4f722671c195476f31ed
Josh Faison <Joshfaiso@yahoo.com>
2020-02-19 10:46:35 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password:'Pepsi0109j',
-    database:'burgers_db'
-})
-
+if (process.env.JAWSDB_RUL) {
+    connection = mysql.createConnection(process.env.JAWSDB_RUL);
+} else {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password:'Pepsi0109j',
+        database:'burgers_db'
+    });
+};
+ 
 connection.connect((err) => {
     if(err) throw err;
     console.log('Connected to port 3306' + connection.threadId);
